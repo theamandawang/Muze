f75dcd5998be062ce4770abba7b189a15a8e4112
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2025-02-20 13:49:32 -0800

    Use upsert for user creation; allow username update

--- muze/src/db/UserUpdate.ts
+++ muze/src/db/UserUpdate.ts
@@ -1,30 +1,53 @@
+'use server';
 import SupabaseClient from './SupabaseClient';
 
 export async function CreateUser(token: any) {
-    const existing = await SupabaseClient.from('users')
-        .select()
-        .eq('id', token.id);
-
-    // if there is no existing
-    if (!existing.error && existing.data.length === 0) {
-        const { data, error } = await SupabaseClient.from('users')
-            .insert({
+    const { data, error } = await SupabaseClient.from('users')
+        .upsert(
+            {
                 bio: null,
                 created_at: new Date().toISOString(),
                 email: token.email,
                 id: token.id,
                 profile_pic: token.picture,
                 username: token.name,
-            })
-            .select();
-
-        if (error) {
-            throw Error('Failed creating new user: ' + error);
-        }
-        console.log('==========Supabase added:');
-        console.log(data);
+            },
+            { onConflict: 'id', ignoreDuplicates: true }
+        )
+        .select();
+    if (error) {
+        console.error(error);
+        throw new Error('Upsert failed! ' + error.message);
     } else {
-        console.log('User exists already!');
-        console.log(existing.data);
+        console.log('==========Supabase upserted:');
+        if (data.length === 0) {
+            console.log('Upserted nothing.');
+        } else {
+            console.log(data);
+        }
+    }
+}
+
+export async function UpdateUsername(id: string, username: string) {
+    const { data, error } = await SupabaseClient.from('users')
+        .update({ username: username })
+        .match({ id: id })
+        .select();
+    if (error) {
+        console.error(error);
+        throw new Error('Error updating username for ' + id);
+    }
+    console.log(data);
+}
+
+export async function UpdateProfilePicture(id: string, username: string) {
+    const { data, error } = await SupabaseClient.from('users')
+        .update({ username: username })
+        .match({ id: id })
+        .select();
+    console.log('Updating the username');
+    console.log(data);
+    if (error) {
+        throw Error('Error updating: ' + error);
     }
 }
