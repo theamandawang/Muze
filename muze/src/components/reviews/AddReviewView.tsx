e33ce5a8d4c11f4b1f91b479197ad2700daa0d3d
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2025-02-25 12:53:28 -0800

    Add temp ui for title and rating

--- muze/src/components/reviews/AddReviewView.tsx
+++ muze/src/components/reviews/AddReviewView.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button'; 
+import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Track } from '@spotify/web-api-ts-sdk';
-import { useSession } from "next-auth/react"
-import { AuthUser } from "@/app/api/auth/[...nextauth]/authOptions";
+import { useSession } from 'next-auth/react';
+import { AuthUser } from '@/app/api/auth/[...nextauth]/authOptions';
 import { addSongReview } from '@/app/api/review/route';
 
 interface AddReviewViewProps {
@@ -17,53 +17,88 @@ interface AddReviewViewProps {
 }
 
 export default function AddReviewView({ track, onBack }: AddReviewViewProps) {
-    const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
     const [title, setTitle] = useState('');
     const [review, setReview] = useState('');
     const trackName = track.name;
     const userId = (session?.user as AuthUser).id;
-    const trackArtistsList = track.artists.map((artist) => artist.name).join(", ");
+    const trackArtistsList = track.artists
+        .map((artist) => artist.name)
+        .join(', ');
     const [rating, setRating] = useState(5);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        addSongReview(userId, track.id, title, review, rating);
+        if (title === '' || review === '') {
+            alert('Please provide a title and a review.');
+        } else {
+            await addSongReview(userId, track.id, title, review, rating);
+        }
     };
 
     return (
-        <Container maxWidth="md">
+        <Container maxWidth='md'>
             <IconButton onClick={onBack} sx={{ mt: 2, mb: 2 }}>
                 <ArrowBackIcon />
             </IconButton>
             <form onSubmit={handleSubmit}>
-                <Box sx={{ 
-                    padding: 3,
-                    borderRadius: 1,
-                    boxShadow: 1
-                }}>
+                <Box
+                    sx={{
+                        padding: 3,
+                        borderRadius: 1,
+                        boxShadow: 1,
+                    }}
+                >
                     <h1>{trackName}</h1>
                     <p>{trackArtistsList}</p>
-                    
+                    <select
+                        onChange={(e) => {
+                            setRating(Number(e.target.value));
+                        }}
+                    >
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                    </select>
+                    <TextField
+                        fullWidth
+                        label='Title'
+                        onChange={(e) => setTitle(e.target.value)}
+                        variant='outlined'
+                        sx={{
+                            mt: 2,
+                            backgroundColor: 'white',
+                        }}
+                    ></TextField>
+
                     <TextField
                         fullWidth
                         multiline
                         rows={4}
-                        label="Write your review..."
+                        label='Write your review...'
                         value={review}
                         onChange={(e) => setReview(e.target.value)}
-                        variant="outlined"
-                        sx={{ 
+                        variant='outlined'
+                        sx={{
                             mt: 2,
-                            backgroundColor: 'white'
+                            backgroundColor: 'white',
                         }}
                     />
-                    
-                    <Box sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
-                        <Button 
-                            type="submit"
-                            variant="contained"
+
+                    <Box
+                        sx={{
+                            mt: 2,
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                        }}
+                    >
+                        <Button
+                            type='submit'
+                            variant='contained'
                             sx={{
-                                minWidth: 120
+                                minWidth: 120,
                             }}
                         >
                             Submit
