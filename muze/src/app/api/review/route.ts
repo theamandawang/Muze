fcf3ad9b86f01c104327b2f3440935c8377755e5
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2025-03-06 15:53:52 -0800

    Remove NextResponse from server actions (#18)
    # Description
    This removes all the `NextResponse` returns from our server actions,
    removing errors from code.
    
    Fixes #16 
    
    ## Type of change
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    
    # How Has This Been Tested?
    
    Tested locally.
    
    # Checklist:
    
    - [X] My code follows the style guidelines of this project
    - [X] I have performed a self-review of my code
    - [X] I have commented my code, particularly in hard-to-understand areas
    - [X] I have made corresponding changes to the documentation
    - [X] My changes generate no new warnings
    - [X] I have added tests that prove my fix is effective or that my
    feature works
    - [X] New and existing unit tests pass locally with my changes
    - [X] Any dependent changes have been merged and published in downstream
    modules

--- muze/src/app/api/review/route.ts
+++ muze/src/app/api/review/route.ts
@@ -1,75 +1,103 @@
-import { NextResponse } from 'next/server';
-import { createSongReview, updateSongReviewByReviewId, deleteSongReviewByReviewId, getLatestSongReviewsAll } from '@/db/songReviews';
+import {
+    createSongReview,
+    updateSongReviewByReviewId,
+    deleteSongReviewByReviewId,
+    getLatestSongReviewsAll,
+} from '@/db/songReviews';
 import getSpotifySongInfo from '@/spotify-api/getSongInfo';
 
-export async function addSongReview(userId:string, trackId:string, title?: string, review?:string, rating?:number) {
+export async function addSongReview(
+    userId: string,
+    trackId: string,
+    title?: string,
+    review?: string,
+    rating?: number
+) {
     if (!userId || !trackId) {
-        return NextResponse.json({ error: 'User ID and Track ID are required' }, { status: 400 }); // status 400: bad request
+        console.error('No User ID and no Track ID');
+        return null;
     }
-    
+
     try {
-        const data = await createSongReview(userId, trackId, title, review, rating);
-        return NextResponse.json({ data }, { status: 200 });
+        const data = await createSongReview(
+            userId,
+            trackId,
+            title,
+            review,
+            rating
+        );
+        return data;
     } catch (error) {
         console.log('Error adding song review:', error);
-        return NextResponse.json({error: 'Failed to add song review'});
+        return null;
     }
 }
 
-export async function updateSongReview(reviewId:string, title?: string, review?:string, rating?:number) {
+export async function updateSongReview(
+    reviewId: string,
+    title?: string,
+    review?: string,
+    rating?: number
+) {
     if (!reviewId) {
-        return NextResponse.json({ error: 'Review ID is required' }, { status: 400 }); // status 400: bad request
+        console.error('No review ID provided.');
+        return null;
     }
-    
+
     try {
-        const data = await updateSongReviewByReviewId(reviewId, title, review, rating);
-        return NextResponse.json({ data }, { status: 200 });
+        const data = await updateSongReviewByReviewId(
+            reviewId,
+            title,
+            review,
+            rating
+        );
+        return data;
     } catch (error) {
         console.log('Error updating song review:', error);
-        return NextResponse.json({error: 'Failed to update song review'});
+        return null;
     }
 }
 
-export async function deleteSongReview(reviewId:string) {
+export async function deleteSongReview(reviewId: string) {
     if (!reviewId) {
-        return NextResponse.json({ error: 'Review ID is required' }, { status: 400 }); // status 400: bad request
+        console.error('No review ID provided.');
+        return null;
     }
 
     try {
         const data = await deleteSongReviewByReviewId(reviewId);
-        return NextResponse.json({ data }, { status: 200 });
+        return data;
     } catch (error) {
         console.log('Error deleting song review:', error);
-        return NextResponse.json({error: 'Failed to delete song review'});
+        return null;
     }
 }
 
-
-
-export async function getLatestSongReviews(limit: number = 50, offset: number = 0) {
-    const data = await getLatestSongReviewsAll(limit, offset); 
+export async function getLatestSongReviews(
+    limit: number = 50,
+    offset: number = 0
+) {
+    const data = await getLatestSongReviewsAll(limit, offset);
     const finalData = await Promise.all(
         data.map(async (review) => {
             // Continue if no song id
             if (review.song_id == null) return {};
 
-            // TODO: Temp workaround to get song data until we implement caching 
+            // TODO: Temp workaround to get song data until we implement caching
             const spotifyData = await getSpotifySongInfo(review.song_id);
 
             // Continue if no song data retrieved
-            if (spotifyData == null) return {}; 
+            if (spotifyData == null) return {};
             return {
                 ...review,
-                mediaType: "Song",
-                mediaName: spotifyData.songName, 
-                artistName: spotifyData.artistNames || "Unknown",
-                mediaCoverArt: spotifyData.imageUrl || "",
+                mediaType: 'Song',
+                mediaName: spotifyData.songName,
+                artistName: spotifyData.artistNames || 'Unknown',
+                mediaCoverArt: spotifyData.imageUrl || '',
                 reviewerName: review.user.username, // Move username to top level
             };
         })
     );
-    
 
     return finalData;
 }
-
