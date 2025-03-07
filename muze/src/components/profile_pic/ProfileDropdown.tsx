f3296927730a5eca9a6e154509d5d6b56d8fb2c9
Maxine Wu <maxinewu5@gmail.com>
2025-03-07 13:41:34 -0800

    add profile dropdown component

---
+++ muze/src/components/profile_pic/ProfileDropdown.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+
+export default function ProfileDropdown() {
+    const handleSignOut = async () => {
+        try {
+            await signOut({ redirect: false, callbackUrl: 'http://localhost:3000/'}); // don't redirect immediately
+        } catch (error) {
+            console.error('Error signing out:', error);
+        } 
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button className="w-full">    
+                    <Image src='/default-profile-pic.svg' alt='profile pic' width={32} height={32}/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-[#1f0f30] absolute top-[10px] right-[-12px] shadow-lg rounded-md p-2 w-40">
+                <DropdownMenuItem>
+                    <Link href="/profile" className="w-full block px-2 py-1">
+                        Profile
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link 
+                        className="w-full text-left px-2 py-1"
+                        href="/"
+                        onClick={() => {handleSignOut()}}
+                    >
+                        Log Out
+                    </Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
