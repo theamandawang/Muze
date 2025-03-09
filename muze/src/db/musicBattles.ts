643bc138361f038ad337a5fd671a6cd8d879ffa9
Venkat Bollapragada <58750962+vbKenobi@users.noreply.github.com>
2025-03-09 14:42:34 -0700

    Music Backend Feature APIs (#25)
    # Description
    
    This PR introduces the APIs requires to support the music battles
    feature, the written APIs include:
      - userAddVote, userRemoveVote 
      - getAllUserVotes, 
      - getAllActiveBattles 
      - getAllVotesForBattle
      - getVotesForArtistInBattle
    
    Relates to issue #22 
    
    ## Type of change
    -  New feature (non-breaking change which adds functionality)
    - This change requires a documentation update
    
    # How Has This Been Tested?
    Tests were run locally to ensure that all APIs behave as expected
    including database modifications, and ensuring that all of the right
    data is returned from the DB.
    
    # Checklist:
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules

---
+++ muze/src/db/musicBattles.ts
@@ -0,0 +1,63 @@
+import { supabase } from '@/lib/supabase/supabase';
+
+// Returns all current music battles 
+export async function getAllActiveBattles()
+{
+    const { data, error } = await supabase.from('music_battles')
+        .select('*')
+        .eq('active', true); 
+    
+    if (error) {
+        throw new Error('Failed to get active music battles.'); 
+    }
+    if (data) {
+        return data; 
+    }
+}
+
+// Returns all votes for a specific music batle
+export async function getAllVotesForBattle(musicBattleId: string)
+{
+    const { data, error } = await supabase.from('music_battle_likes')
+        .select('*')
+        .eq('battle_id', musicBattleId);
+
+    if (error) {
+        throw new Error('Failed to get votes for battle')
+    }
+    if (data) {
+        return data; 
+    }
+}
+
+// Returns all votes for a specific music batle and specific artist
+export async function getAllVotesForArtistInBattle(musicBattleId: string, artistId: string)
+{
+    const { data, error } = await supabase.from('music_battle_likes')
+        .select('*')
+        .eq('battle_id', musicBattleId)
+        .eq('artist_vote', artistId); 
+
+    if (error) {
+        throw new Error('Failed to get votes for battle')
+    }
+    if (data) {
+        return data; 
+    }
+}
+
+
+// Add a vote for a specific artist within a specific music battle 
+// Assumes the U.I. has toggle control
+export async function userAddVote(musicBattleId: string, userId: string, artistId: string) 
+{
+    const { error } = await supabase.from('music_battle_likes').insert([
+        {
+            battle_id: musicBattleId, 
+            user_id: userId, 
+            artist_vote: artistId,
+        }
+    ]);
+
+    if (error) throw error; 
+}
